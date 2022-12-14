c4d37424549adbb157baa2ee259c137ad1d919ce
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-12-14 23:34:46 +0200

    styled button logout

--- src/components/UserMenu/UserMenu.styled.js
+++ src/components/UserMenu/UserMenu.styled.js
@@ -10,5 +10,14 @@ export const Wrapper = styled.div`
 export const Greeting = styled.p`
   font-weight: 700;
 `;
-export const Button = styled.button``;
+export const Button = styled.button`
+  margin-right: 30px;
+  width: 80px;
+  height: 30px;
+  border: none;
+  border-radius: 10px;
+  background-color: #000000;
+  color: white;
+  cursor: pointer;
+`;
 
